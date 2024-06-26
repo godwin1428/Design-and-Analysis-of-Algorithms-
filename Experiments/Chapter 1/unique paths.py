90e2d48601ed74ad99d6f5250b2645086180dce4
T Godwin Abilash <154417008+godwin1428@users.noreply.github.com>
2024-06-26 13:07:58 +0530

    Create unique paths.py

---
+++ Experiments/Chapter 1/unique paths.py
@@ -0,0 +1,12 @@
+def unique_paths(m, n):
+    dp = [[0] * n for _ in range(m)]
+    for i in range(m):
+        dp[i][0] = 1
+    for j in range(n):
+        dp[0][j] = 1
+    for i in range(1, m):
+        for j in range(1, n):
+            dp[i][j] = dp[i-1][j] + dp[i][j-1]
+    return dp[m-1][n-1]
+print(unique_paths(7, 3))  
+print(unique_paths(3, 2)) 
